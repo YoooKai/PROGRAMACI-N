c33343d7908156d006cb645b06daa0fdb4a3455c
Kai <145053705+YoooKai@users.noreply.github.com>
2024-05-06 12:35:02 +0100

    Add files via upload

---
+++ Objetos/Ejer1/fibonacci_iterable.py
@@ -0,0 +1,22 @@
+# ******************
+# FIBONACCI ITERABLE
+# ******************
+
+class Fibonacci:
+    def __init__(self, n):
+        self.n = n
+        self.fibo = [0, 1]
+        self.index = 0
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.index < self.n:
+            if self.index >= len(self.fibo):
+                self.fibo.append(self.fibo[-1] + self.fibo[-2])
+            result = self.fib_sequence[self.index]
+            self.index += 1
+            return result
+        else:
+             raise StopIteration
